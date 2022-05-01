e507c89773fd4923dca5895ca668f31d754b9c98
Sean Lang <slang800@gmail.com>
2022-05-01 12:22:41 -0500

    solve euler problem 5

---
+++ euler5.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import sys
+
+
+def get_int_input(): return int(input().strip())
+
+
+def is_divisible_by_range(number, a, b):
+    for divisor in range(b, a - 1, -1):
+        if number % divisor != 0:
+            return False
+
+    return True
+
+
+def handle_test_case(n):
+    """
+    Handle the test case.
+
+    >>> handle_test_case(3)
+    6
+    >>> handle_test_case(10)
+    2520
+    """
+    test_number = 1
+
+    while True:
+        if is_divisible_by_range(test_number, 1, n):
+            return test_number
+
+        test_number += 1
+
+
+if __name__ == '__main__':
+    number_of_tests = get_int_input()
+    for a0 in range(number_of_tests):
+        n = get_int_input()
+        res = handle_test_case(n)
+        print(res)
